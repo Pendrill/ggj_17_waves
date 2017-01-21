e4a49cef56939d12f6ff022793802fa1ffba0089
Calvin So <ccs403@nyu.edu>
2017-01-21 14:15:52 -0500

    Spawning but no death boxes yet

--- Assets/Scripts/Temp_Scripts/Game_Manager_Aleksei.cs
+++ Assets/Scripts/Temp_Scripts/Game_Manager_Aleksei.cs
@@ -5,13 +5,69 @@ using UnityEngine;
 public class Game_Manager_Aleksei : MonoBehaviour {
 	public int Player1_Score;
 	public int Player2_Score;
+    public List<string> ValidLetters;
+    public List<GameObject> Students;
+    public List<float> ValidYPositions;
+
+    private Vector3 leftPosition = new Vector3(-10, 0, 0);
+    private Vector3 rightPosition = new Vector3(10, 0, 0);
+
+    private IEnumerator coroutine;
+    private bool SpawningStudent = true;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        StartCoroutine(SpawnStudent());
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
+
+    private IEnumerator SpawnStudent()
+    {
+        while (SpawningStudent)
+        {
+           
+            //Pick left or right side to spawn.
+            int leftOrRight = Random.Range(0, 2);
+            Vector3 SpawnPosition;
+            if (leftOrRight == 0)
+            {
+                SpawnPosition = leftPosition;
+            }
+            else
+            {
+                SpawnPosition = rightPosition;
+            }
+
+            //Pick one of the valid heights and add to the y of SpawnPosition
+            int randomYValue = Random.Range(0, ValidYPositions.Count);
+            SpawnPosition.y += ValidYPositions[randomYValue];
+
+            //Pick a random student model and spawn at position.
+            int index = Random.Range(0, Students.Count);
+            GameObject temp = Instantiate(Students[index], SpawnPosition, Quaternion.identity);
+
+            //Set the move direction on the spawned student to the correct one.
+            temp.GetComponent<Move_Student>().moveDirection = (Move_Student.Direction) leftOrRight;
+            temp.GetComponent<Move_Student>().Student_Letter = GetRandomLetter();
+            yield return new WaitForSeconds(2.0f);
+        }
+       
+    }
+
+    string GetRandomLetter()
+    {
+        int index = Random.Range(0, ValidLetters.Count);
+        string letter = ValidLetters[index];
+        ValidLetters.Remove(letter);
+        return letter;
+    }
+
+    void ReturnLetter(string letter)
+    {
+        ValidLetters.Add(letter);
+    }
+
 }
--- Assets/Scripts/Temp_Scripts/Move_Student.cs
+++ Assets/Scripts/Temp_Scripts/Move_Student.cs
@@ -3,23 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move_Student : MonoBehaviour {
-	public Rigidbody2D Student_RigidBody;
-	public float Student_Speed;
-	public string Student_Letter;
-	public int p1_score, p2_score;
-	public int Exit_Counter;
+    public Rigidbody2D Student_RigidBody;
+    public float Student_Speed;
+    public string Student_Letter;
+    public int p1_score, p2_score;
+    public int Exit_Counter;
+    public enum Direction {Left, Right};
+    public Direction moveDirection;
 	// Use this for initialization
 	void Start () {
 		Exit_Counter = 0;
-		Student_Speed = 3;
 		Student_RigidBody = GetComponent<Rigidbody2D> ();
 		//Student_Letter = "m";
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Student_RigidBody.AddForce (-transform.right * Student_Speed * Time.deltaTime);
-		//transform.position += new Vector3(-0.1f, 0, 0);
+		//Student_RigidBody.AddForce (-transform.right * Student_Speed * Time.deltaTime);
+        if(moveDirection == Direction.Left)
+        {
+            transform.position += new Vector3(Student_Speed, 0, 0);
+        }
+        else if (moveDirection == Direction.Right)
+        {
+            transform.position += new Vector3(-Student_Speed, 0, 0);
+        }
+		
 	}
 	public void increase_Score(int player_score, int which_student){
 		Debug.Log ("Increase score is being accessed");
