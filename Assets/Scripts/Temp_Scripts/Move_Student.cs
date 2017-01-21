88a356e3dd7b0609d4e63b9029c8d3262adfc5b5
Aleksei Waddington <amw694@nyu.edu>
2017-01-21 12:32:23 -0500

    prototyping movement and collision with the player's wave area.

---
+++ Assets/Scripts/Temp_Scripts/Move_Student.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Student : MonoBehaviour {
+	public Rigidbody2D Student_RigidBody;
+	public float Student_Speed;
+	// Use this for initialization
+	void Start () {
+		Student_Speed = 3;
+		Student_RigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Student_RigidBody.AddForce (-transform.right * Student_Speed * Time.deltaTime);
+		//transform.position += new Vector3(-0.1f, 0, 0);
+	}
+
+
+}
